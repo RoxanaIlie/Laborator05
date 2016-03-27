7a7ea8b208e845020423ab7a47cc3838b5e4b63b
Roxana Ilie <roxana.ilie23@gmail.cm>
2016-03-27 16:34:59 +0300

    implemented tasks for lab05

--- labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/eclipse/StartedService/StartedServiceActivity/src/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -3,6 +3,7 @@ package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.SyncStateContract.Constants;
 import android.widget.TextView;
 
 public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
@@ -19,9 +20,27 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: exercise 6 - get the action and the extra information from the intent
         // and set the text on the messageTextView
+    	String action = intent.getAction();
+    	String data = null;
+    	
+        if (ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.ACTION_STRING.equals(action)) {
+        	data = intent.getStringExtra(Constants.DATA);
+        } else if (ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.ACTION_INTEGER.equals(action)) {
+        	data = String.valueOf(intent.getIntExtra(ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.DATA, -1));
+        } else if (ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.ACTION_ARRAY_LIST.equals(action)) {
+        	data = intent.getStringArrayExtra(ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.DATA).toString();
+        }
 
+        if (messageTextView != null)
+        	messageTextView.setText(messageTextView.getText().toString() + "\n" + data);
+        
+        
         // TODO: exercise 8 - restart the activity through an intent
         // if the messageTextView is not available
+        Intent startedServiceActivityIntent = new Intent(context, StartedServiceActivity.class);
+        startedServiceActivityIntent.putExtra(ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants.MESSAGE, data);
+        startedServiceActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(startedServiceActivityIntent);
     }
 
 }
